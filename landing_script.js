9ca895940a747906b7cfbd4b81a6515cbdb6a069
Olli Myyryläinen <98151462+ollimyy@users.noreply.github.com>
2024-01-03 15:39:24 +0200

    Add landing page. Improve project structure and filenames.

---
+++ landing_script.js
@@ -0,0 +1,8 @@
+// handle rules modal visibility
+document.getElementById('open-rules-button').onclick = function() {
+    document.getElementById('rules-modal').style.display = 'flex';
+}
+
+document.getElementById('close-rules-button').onclick = function() {
+    document.getElementById('rules-modal').style.display = 'none';
+}
